29c331a94d0b573ad22351ff94093cbe936f8cbf
Wenbao Xu <mail@xuwenbao.me>
2019-01-18 18:37:47 +0800

    init

---
+++ install-kubeadm.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kube*
+EOF
+
+# Set SELinux in permissive mode (effectively disabling it)
+setenforce 0
+sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+yum install -y kubeadm --disableexcludes=kubernetes
